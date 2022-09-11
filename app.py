7057ffe03bc08ccb38f6608aea7ed16252e6077e
Vishal <vishal@1MG-LPTP-1341.local>
2022-09-12 00:12:32 +0530

    Adding comments

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Request
 from managers import TodoManager
 from redis_om import Migrator
 from flask_cors import CORS
@@ -8,34 +8,58 @@ CORS(app)
 
 @app.route('/ping')
 def ping():
+    """
+    Ping API
+    :return: string
+    """
     return 'pong'
 
 
 @app.route('/redis-todo', strict_slashes=False)
 def home_page():
+    """
+    Serves the home page
+    :return:
+    """
     return render_template('index.html')
 
 
 @app.route('/todo', methods=["POST"])
 def set_todo():
+    """
+    Set a new to do item.
+    :return: Result
+    """
     result = TodoManager().set_todo(request.json)
     return result.dict()
 
 
 @app.route('/todos', methods=["GET"])
 def get_todos():
+    """
+    Get all todo items
+    :return: Result
+    """
     result = TodoManager().get_todo(request)
     return result.dict()
 
 
 @app.route('/todo', methods=["PUT"])
 def upsert_todos():
+    """
+    Update a given todo item.
+    :return: Result
+    """
     result = TodoManager().upsert_todo(request.json)
     return result.dict()
 
 
 @app.route('/todos', methods=["DELETE"])
 def delete_todos():
+    """
+    Delete todo items in bulk whose status is active
+    :return:
+    """
     result = TodoManager().delete_todos()
     return result.dict()
 
--- managers/todomanager.py
+++ managers/todomanager.py
@@ -5,6 +5,11 @@ from pydantic import ValidationError
 
 class TodoManager:
     def set_todo(self, payload) -> Result:
+        """
+        Set a new todo item in redis.
+        :param payload: {'item': string}
+        :return: Result
+        """
         item = payload.get('item')
         if not item:
             return Result(data=None, status=False, message=Messages.ERROR)
@@ -16,6 +21,11 @@ class TodoManager:
         return Result(data=new_sku, status=True, message=Messages.SET_SUCCESS)
 
     def get_todo(self, request) -> Result:
+        """
+        Given a filter value, returns all todo present in redis.
+        :param request: {'filter'='all|active|completed'}
+        :return: Result
+        """
         filter_value = request.args.get('filter')
         query = TodoItem.find()
         if filter_value == 'active':
@@ -30,6 +40,11 @@ class TodoManager:
         return Result(data=items, status=True, message=Messages.GET_SUCCESS)
 
     def upsert_todo(self, payload) -> Result:
+        """
+        Upsert an item in redis
+        :param payload: {'pk': string, 'item': string, 'status': string}
+        :return: Result
+        """
         try:
             item = TodoItem(**payload)
             item.save()
@@ -38,6 +53,10 @@ class TodoManager:
         return Result(data=None, status=True, message=Messages.UPSERT_SUCCESS)
 
     def delete_todos(self) -> Result:
+        """
+        Delete completed items in bulk.
+        :return: Result
+        """
         items = TodoItem.find(
             TodoItem.status == 'completed'
         ).all()
